d909560ac99c71624b28b7fa188dc481b57164e0
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-05-27 07:48:06 +0200

    simple parser check

--- src/test/java/de/hechler/patrick/Fileparser.java
+++ src/test/java/de/hechler/patrick/Fileparser.java
@@ -1,9 +1,12 @@
 package de.hechler.patrick;
 
+import java.util.Iterator;
+
 import de.hechler.patrick.fileparser.MAINChecker;
 import de.hechler.patrick.fileparser.ParserChecker;
 import de.hechler.patrick.zeugs.check.Checker;
 import de.hechler.patrick.zeugs.check.Checker.BigCheckResult;
+import de.hechler.patrick.zeugs.check.exceptions.CheckerNotEqualsExeption;
 
 public class Fileparser {
 	
@@ -12,6 +15,22 @@ public class Fileparser {
 		res.print();
 		res.forAllUnexpected((c, m, t) -> {
 			System.err.println(c + "  ->  " + m + ":");
+			if (t instanceof CheckerNotEqualsExeption) {
+				CheckerNotEqualsExeption cnee = (CheckerNotEqualsExeption) t;
+				System.err.println("a.hash=" + cnee.a.hashCode());
+				System.err.println("b.hash=" + cnee.b.hashCode());
+				if (cnee.a.getClass() == String.class && cnee.b.getClass() == String.class) {
+					char[] a = ((String) cnee.a).toCharArray(), b = ((String) cnee.b).toCharArray();
+					if (a.length != b.length) System.err.println("diffrent lenght: a=" + a.length + " b=" + b.length);
+					for (int i = 0; i < a.length && i < b.length; i ++ ) {
+						if (a[i] != b[i]) {
+							String as = a[i] == '\r' ? "[CR]" : (a[i] == '\n' ? "[LF]" : (a[i] == '\t' ? "[TAB]" : a[i] == ' ' ? "[ ]" : (a[i] + ""))),
+									bs = b[i] == '\r' ? "[CR]" : (b[i] == '\n' ? "[LF]" : (b[i] == '\t' ? "[TAB]" : b[i] == ' ' ? "[ ]" : (b[i] + "")));
+							System.err.println("a[" + i + "]=" + as + " b[" + i + "]=" + bs);
+						}
+					}
+				}
+			}
 			t.printStackTrace();
 		});
 	}
--- src/test/java/de/hechler/patrick/fileparser/ParserChecker.java
+++ src/test/java/de/hechler/patrick/fileparser/ParserChecker.java
@@ -1,35 +1,62 @@
 package de.hechler.patrick.fileparser;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import de.hechler.patrick.fileparser.Parser.ParserTemplate;
 import de.hechler.patrick.zeugs.check.*;
 import de.hechler.patrick.zeugs.check.anotations.*;
 
 @CheckClass
-//TODO checks
+// TODO more checks
 public class ParserChecker extends Checker {
+	
+	ByteArrayInputStream  bais;
+	Scanner               scanner;
+	ByteArrayOutputStream baos;
+	PrintStream           print;
+	ParserTemplate        template;
+	Parser                parser;
 	
 	@Start(onlyOnce = true)
 	void beginAll() {
-		
 	}
 	
-	@End(onlyOnce =  true)
+	@End(onlyOnce = true)
 	void endAll() {
-		
 	}
 	
 	@Start
 	void start() {
-		
+		bais = null;
+		scanner = null;
+		baos = new ByteArrayOutputStream();
+		print = new PrintStream(baos, false /* the checks will decide when it will flush */, StandardCharsets.UTF_8);
+		template = Parser.EMPTY_TEMPLATE;
+		parser = new Parser(template);
 	}
-		
+	
 	@End
 	void end() {
-		
+		bais = null;
+		scanner = null;
+		baos = null;
+		print = null;
+		template = null;
+		parser = null;
 	}
 	
 	@Check
-	void check() {
-		
+	void checkEmptyTemplate() {
+		String str = "this is a nice text.\nIt will be unchanged!\n\n\t\t\n  \n".replaceAll("(\r\n)|\r|\n", "\\\r\\\n");
+		bais = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
+		scanner = new Scanner(bais, StandardCharsets.UTF_8);
+		parser.parse(scanner, print);
+		print.flush();
+		assertEquals(str, new String(baos.toByteArray(), StandardCharsets.UTF_8));
 	}
 	
 }
